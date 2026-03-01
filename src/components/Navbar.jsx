10596813bf16dda5bb3ca4260b861125e7618c10
patties-eater <anjugautam7223@gmail.com>
2026-03-02 00:28:28 +0545

    call work well

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,6 +72,7 @@ export default function Navbar() {
                   : "text-white hover:text-pink-200 w-full"
               }
             >
+              
               {link.name}
             </NavLink>
           ))}
